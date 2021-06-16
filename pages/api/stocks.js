3b888f67d90bb82bd8d50b2217aa73030f02997d
thomasnascimento <thomasnsilva11@gmail.com>
2021-06-16 07:14:24 -0300

    teste api apidojo

--- pages/api/stocks.js
+++ pages/api/stocks.js
@@ -1,21 +1,28 @@
-const settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=ABEV3.SA&region=BR",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-key": "658d1c4a73msh4e30ca46c927af4p143700jsnee22c5ecae2f",
-        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
-    }
-};
+async function tempo(request, response) {
+    const dynamicDate = new Date();
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
-})
+    const subscribersResponse = await fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=ABEV3.SA&region=BR", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "658d1c4a73msh4e30ca46c927af4p143700jsnee22c5ecae2f",
+            "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
+        }
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+    const subscribersResponseJson = await subscribersResponse.json();
+    const inscritos = subscribersResponseJson;
 
-function HistoricalData() {
-    return (
-        <div>HistoricalData - ABEV3</div>
-    )
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
+    response.json({
+        date: dynamicDate.toGMTString(),
+        inscritos: inscritos
+    });
 }
-export default HistoricalData
+
+export default tempo;
