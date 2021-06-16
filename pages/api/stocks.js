7b86730ee0b2c5da7faeac35bf12069be595a5ac
thomasnascimento <thomasnsilva11@gmail.com>
2021-06-16 08:37:33 -0300

    teste api apidojo

--- pages/api/stocks.js
+++ pages/api/stocks.js
@@ -15,7 +15,7 @@ async function tempo(request, response) {
         console.error(err);
     });
     const subscribersResponseJson = await subscribersResponse.json();
-    const inscritos = subscribersResponseJson.firstTradeDate;
+    const inscritos = subscribersResponseJson.prices;
 
     response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
 
